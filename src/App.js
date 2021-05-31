d6bef19b824372c6535e2dd93f909742a15735a9
Amisha Singh <singhamisha.871@gmail.com>
2021-05-31 20:48:16 +0530

    Fully functioned cards chart

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import {Cards,Chart,CountryPicker}from './components';
-import styles from './App.module.css';
-import { fetchData } from './api';
+import React from 'react'
+import { Cards, Chart, CountryPicker } from './components'
+import styles from './App.module.css'
+import { fetchData } from './api'
+
 class App extends React.Component {
+
   state = {
-    data:{},
+    data: {},
+    country: '',
   }
-  async componentDidMount(){
-    const fetchedData =await fetchData();
-    this.setState({data: fetchedData});
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchedData });
   }
-  render(){
-    const { data}=this.state;
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    this.setState({ data: fetchedData, country: country });
+  }
+
+  render() {
+    const { data, country } = this.state;
     return (
       <div className={styles.container}>
-        <Cards data={data}/>
-        <CountryPicker />
-        <Chart />
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
       </div>
-    )
+    );
   }
 }
+
 export default App;
